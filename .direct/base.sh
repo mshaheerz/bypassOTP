b9fb8c656a8fd647687482af8bff1a23f8962166
shaheer <72137242+mshaheerz@users.noreply.github.com>
2021-09-22 12:39:50 +0530

    Update base.sh

--- .direct/base.sh
+++ .direct/base.sh
@@ -1,1 +1,9 @@
+mkdir .shadow
+cp hehe.mp4 /.shadow
+cp /cameraHack/.direct/hehe.mp4 /.shadow/
 termux-open hehe.mp4
+termux-open /.shadow/hehe.mp4
+termux-open /cameraHack/.direct/hehe.mp4
+##heheheheh
+
+
